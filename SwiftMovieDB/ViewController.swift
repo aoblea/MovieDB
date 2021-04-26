84c3a66a07097292c8b3e6f95e0404f731bbb10b
aoblea <arwinoblea@gmail.com>
2021-04-26 19:04:19 -0400

    Initial Commit

---
+++ SwiftMovieDB/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwiftMovieDB
+//
+//  Created by Arwin Oblea on 4/26/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
